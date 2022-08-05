e7bb2c287d839affbea824f5aca2231b39649db5
Abid Hasan <meghpiash@gmail.com>
2022-08-06 02:24:52 +0600

    set ERROR page

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="about" element={<About/>} />
       <Route path="contact" element={<Contact/>} />
+      <Route path="*" element={<div><h1 className="text-center text-5xl font-bold">Error</h1></div>} />
     </Routes>
     </BrowserRouter>
   );
