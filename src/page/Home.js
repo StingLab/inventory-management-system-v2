cec3f41c5b3a8b17b98e48f374053926f4c69be8
Abid Hasan <meghpiash@gmail.com>
2022-08-05 23:44:38 +0600

    Home,About,Contact page filled with dummy things

--- src/page/Home.js
+++ src/page/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../component/Header';
+import HomeLogin from '../component/HomeLogin';
 
 function Home(props) {
     return (
         <div>
             <Header/>
+            <HomeLogin/>
         </div>
     );
 }
