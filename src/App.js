58a3a298d32ae947285c3b18b20b43e32568ffbf
Abid Hasan <meghpiash@gmail.com>
2022-08-06 23:35:37 +0600

    optimise react router for app.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import About from "./page/About";
 import Contact from "./page/Contact";
 import Home from "./page/Home";
 
 function App() {
   return (
-    <BrowserRouter>
-    <Routes>
+    <div>
+      <Routes>
       <Route path="/" element={<Home />} />
       <Route path="about" element={<About/>} />
       <Route path="contact" element={<Contact/>} />
-      <Route path="*" element={<div><h1 className="text-center text-5xl font-bold">Error</h1></div>} />
-    </Routes>
-    </BrowserRouter>
+      <Route path="*" element={<div><h1 className="text-center text-5xl font-bold">404 Not Found</h1></div>} />
+      </Routes>
+    </div>
   );
 }
 
