790c282b984c98504a42989835c64799498bd918
Abid Hasan <meghpiash@gmail.com>
2022-08-08 02:11:00 +0600

    MUI init

--- src/component/HeaderAndSidebar.js
+++ src/component/HeaderAndSidebar.js
@@ -32,7 +32,7 @@ function HeaderAndSidebar(props) {
                 </div>
             </div> 
 
-            <div className="drawer drawer-mobile">
+            <div className="drawer drawer-left">
                 <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
                 <div className="drawer-content flex flex-col items-center justify-center">
                     <label for="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">Open drawer</label>
