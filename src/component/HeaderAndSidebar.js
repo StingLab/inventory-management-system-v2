00ec6753688b4ecf00b18f3d6d9bbaebb35e596f
Abid Hasan <meghpiash@gmail.com>
2022-08-08 03:35:26 +0600

    set Drawer to deshboard

--- src/component/HeaderAndSidebar.js
+++ src/component/HeaderAndSidebar.js
@@ -32,7 +32,7 @@ function HeaderAndSidebar(props) {
                 </div>
             </div> 
 
-            <div className="drawer drawer-left">
+            <div className="drawer drawer-mobile">
                 <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
                 <div className="drawer-content flex flex-col items-center justify-center">
                     <label for="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">Open drawer</label>
